9b944f2117395eb56f24d89d8d6c9f64c1e62667
thitikl <78643158+thitikl@users.noreply.github.com>
2021-02-24 19:40:54 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+import art
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+print(art.logo)
+
+end_program = False
+while not end_program:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+
+  if shift > 26:
+    shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+  
+  choice = input("Type 'yes' if you want to keep program running, type 'no' if you want to end\n")
+  if choice == "no":
+    print("Goodbye")
+    end_program = True
